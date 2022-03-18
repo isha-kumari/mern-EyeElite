4cc65bfc28e9086a80421a17bb87c6acd1df16fd
Isha Kumari <isha81023@gmail.com>
2022-03-17 22:27:46 -0700

    React-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">EyeElite</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
